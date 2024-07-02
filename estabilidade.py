29810ca529769da9943047700e4c5d73684823da
Jean <jean_thomasio@usp.br>
2024-07-01 21:14:17 -0300

    Commit de merge final

---
+++ estabilidade.py
@@ -0,0 +1,14 @@
+import aircraftconceptualdesign as acd
+
+aviao = {
+    'asa': {
+        'b': 21.0,
+        'c_root': 2.743,
+        'c_tip': 1.920,
+    },
+    'empenagem': {
+        'b': 3.250 * 2,
+        'c_root': 1.732,
+        'c_tip': 1.039
+    }
+}
